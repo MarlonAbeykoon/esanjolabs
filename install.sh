f03c69fa141fa1795b5924e360c71f595f38dcec
marlonabeykoon <marlonabeykoon@gmail.com>
2018-07-04 11:04:24 +0530

    updated install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 pip install virtualenv
 virtualenv -p /usr/bin/python3.5  venv
+echo 'export FLASK_APP=app/app.py' >> ./venv/bin/activate
 source ./venv/bin/activate
 sudo venv/bin/pip install -r requirement.txt
 python manage.py createdb -t
-export FLASK_APP=app/app.py
